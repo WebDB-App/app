9fad5cf9312cd499f8d590c8bf6a65e69904e063
biche <aleluff@gmail.com>
2023-06-04 15:04:07 +0200

    building_construction: Add route data for route reuse
    Add route data to each route in the `CoreRoutingModule` file to indicate that the routes should be reused. This is done by setting the `reuseRoute` property to `true` in the `data` object for each route.
    
    :recycle: Modify CustomReuseStrategy to handle route reuse
    
    Update the `CustomReuseStrategy` class in the `custom-reuse-strategy.ts` file to support the reuse of specific routes. The `CustomReuseStrategy` class implements the `RouteReuseStrategy` interface from the `@angular/router` module.
    
    - Modify the `shouldDetach` method to return `true` if the route has the `reuseRoute` property set to `true`, indicating that it should be reused.
    - Modify the `store` method to store the detached route handle only if the `reuseRoute` property is set to `true`.
    - Modify the `shouldAttach` method to check if the route has a valid route configuration and a stored handle for the given route.
    - Modify the `retrieve` method to return the stored handle for the given route if it exists and has a valid route configuration.
    - Add a private `createIdentifier` method to generate a unique identifier for each route based on its path and segments.
    
    These changes enable the reuse of routes with the specified `reuseRoute` property set to `true`, allowing for improved performance and user experience.

--- front/src/app/custom-reuse-strategy.ts
+++ front/src/app/custom-reuse-strategy.ts
@@ -1,30 +1,40 @@
-import {
-	ActivatedRouteSnapshot,
-	DetachedRouteHandle,
-	RouteReuseStrategy,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy, UrlSegment, } from '@angular/router';
 
 export class CustomReuseStrategy implements RouteReuseStrategy {
-	routesToCache: string[] = ['explore', 'query', 'structure', 'insert', 'trigger', 'advanced'];
-	storedRouteHandles = new Map<string, DetachedRouteHandle>();
+	storedHandles: { [key: string]: DetachedRouteHandle } = {};
 
 	shouldDetach(route: ActivatedRouteSnapshot): boolean {
-		return this.routesToCache.indexOf(route.routeConfig!.path!) > -1;
+		return route.data['reuseRoute'] || false;
 	}
 
 	store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-		this.storedRouteHandles.set(route.routeConfig!.path!, handle);
+		const id = this.createIdentifier(route);
+		if (route.data['reuseRoute']) {
+			this.storedHandles[id] = handle;
+		}
 	}
 
 	shouldAttach(route: ActivatedRouteSnapshot): boolean {
-		return this.storedRouteHandles.has(route.routeConfig!.path!);
+		const id = this.createIdentifier(route);
+		const handle = this.storedHandles[id];
+		return !!route.routeConfig && !!handle;
 	}
 
-	retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
-		return this.storedRouteHandles.get(route.routeConfig!.path!)!;
+	retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
+		const id = this.createIdentifier(route);
+		if (!route.routeConfig || !this.storedHandles[id]) {
+			return null;
+		}
+		return this.storedHandles[id];
 	}
 
 	shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
 		return future.routeConfig === curr.routeConfig;
+	}
+
+	private createIdentifier(route: ActivatedRouteSnapshot) {
+		const segments: UrlSegment[][] = route.pathFromRoot.map(r => r.url);
+		const subpaths = ([] as UrlSegment[]).concat(...segments).map(segment => segment.path);
+		return segments.length + '-' + subpaths.join('/');
 	}
 }
