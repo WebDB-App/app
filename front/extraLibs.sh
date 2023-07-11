a204a91c3a2e3fe098495cfc5277f00ea0bfb5d5
biche <aleluff@gmail.com>
2023-07-11 11:02:54 +0200

    lol

--- front/extraLibs.sh
+++ front/extraLibs.sh
@@ -2,8 +2,8 @@
 
 #npm i -g dts-bundle-generator
 
-cd ./node_modules/mongodb/ && dts-bundle-generator ./mongodb.d.ts && cp -fr mongodb.d.d.ts ../../../../../src/assets/libs/mongo.d.ts
+cd ./node_modules/mongodb/ && dts-bundle-generator ./mongodb.d.ts && cp -fr mongodb.d.d.ts ../../../../../src/assets/libs/mongo.d.ts &
 
-cd ./node_modules/bson/ && dts-bundle-generator ./bson.d.ts && cp -fr bson.d.d.ts ../../../../../src/assets/libs/bson.d.ts
+cd ./node_modules/bson/ && dts-bundle-generator ./bson.d.ts && cp -fr bson.d.d.ts ../../../../../src/assets/libs/bson.d.ts &
 
-cd ./node_modules/@faker-js/faker && dts-bundle-generator --no-check ./dist/types/index.d.ts --external-inlines=@faker-js/faker && cp -fr ./dist/types/index.d.d.ts ../../../../../../src/assets/libs/faker.d.ts
+cd ./node_modules/@faker-js/faker && dts-bundle-generator --no-check ./dist/types/index.d.ts --external-inlines=@faker-js/faker && cp -fr ./dist/types/index.d.d.ts ../../../../../../src/assets/libs/faker.d.ts &
