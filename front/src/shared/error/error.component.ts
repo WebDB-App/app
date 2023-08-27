f1eb8cc0e4a5aaa1a53fa79ac859ebe3bc21cd80
biche <aleluff@gmail.com>
2023-08-27 11:22:38 +0200

    lol

---
+++ front/src/shared/error/error.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from "@angular/material/bottom-sheet";
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss']
+})
+export class ErrorComponent implements OnInit {
+
+	constructor(
+		public bottomSheetRef: MatBottomSheetRef<ErrorComponent>,
+		@Inject(MAT_BOTTOM_SHEET_DATA) public error: any
+	) {}
+
+	ngOnInit() {
+
+	}
+
+	async assistant() {
+		/*this.drawer.toggle();
+		const question = 'When running this query "' + helper.removeComment(this.query) + '" , I got this : ' + JSON.stringify(row) + ', can you fix it for me';
+		await this.router.navigate(
+			[{outlets: {right: ['assistant', {question}]}}],
+			{relativeTo: this.activatedRoute.parent?.parent})*/
+	}
+}
