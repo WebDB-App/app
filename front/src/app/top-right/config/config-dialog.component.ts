3dcc93d1d8160776207c0eafc3b02746613b5d9c
biche <aleluff@gmail.com>
2023-09-21 21:31:52 +0200

    fix(.gitlab-ci.yml): update changeLog.sh command to generate changelog.html with a specific commit hash to ensure accurate changelog generation chore(README.md): remove unnecessary lines and fix formatting fix(front): remove unnecessary content from changelog.html file feat(front): update config-dialog.component.html to display version information and check for updates feat(front): update config-dialog.component.ts to fetch changelog.html from local and remote sources and determine if app is up to date

--- front/src/app/top-right/config/config-dialog.component.ts
+++ front/src/app/top-right/config/config-dialog.component.ts
@@ -8,6 +8,8 @@ import { Licence } from "../../../classes/licence";
 import { environment } from "../../../environments/environment";
 import { MatDialogRef } from "@angular/material/dialog";
 import packageJson from '../../../../package.json';
+import { firstValueFrom } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
 	templateUrl: './config-dialog.component.html',
@@ -15,22 +17,31 @@ import packageJson from '../../../../package.json';
 })
 export class ConfigDialog implements OnInit {
 
+	upToDate!: boolean;
 	configuration: Configuration = new Configuration();
 	email = new FormControl('', [Validators.required, Validators.email]);
 	licence?: Licence;
 	env = environment
 	protected readonly packageJson = packageJson;
 
-
 	constructor(
 		public dialogRef: MatDialogRef<ConfigDialog>,
 		private snackBar: MatSnackBar,
+		private http: HttpClient,
 		private request: RequestService
 	) {
 	}
 
 	async ngOnInit() {
 		this.licence = await Licence.get(this.request, false);
+
+		const local = await firstValueFrom(
+			this.http.get(`${environment.rootUrl}changelog.html`, {responseType: 'text'})
+		);
+		const remote = await firstValueFrom(
+			this.http.get(`https://demo.webdb.app/changelog.html`, {responseType: 'text'})
+		);
+		this.upToDate = local.length >= remote.length;
 	}
 
 	async save(email: string) {
