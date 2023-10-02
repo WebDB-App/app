6806084ee13e4f5f77cd505e49794a5d67ba057e
biche <aleluff@gmail.com>
2023-10-02 11:04:18 +0200

    fix(.gitlab-ci.yml): add echo command to prevent failure when building on non-main branches fix(.gitlab-ci.yml): add echo command to prevent failure when building on non-main branches fix(.eslintrc.cjs): change quotes rule severity from error to warn feat(index.js): add Sentry error tracking and request tracing middleware in production environment fix(mysql.js): change nameDel assignment to use template literal for consistency fix(app.module.ts): add Sentry error handler for Angular application fix(config-dialog.component.html): update link to installation page fix(config-dialog.component.html): add copyright notice feat(config-dialog.component.ts): add currentYear property to display current year in copyright notice fix(top-right.component.html): change settings icon to display_settings feat(main.ts): initialize Sentry error tracking in Angular application

--- back/src/index.js
+++ back/src/index.js
@@ -4,12 +4,28 @@ import express from "express";
 import cors from "cors";
 import {promises as fsp} from "fs";
 import bash from "./shared/bash.js";
+import * as Sentry from "@sentry/node";
 
 const dirname = new URL(".", import.meta.url).pathname;
 dotenv.config({path: dirname + "/../.env"});
 
 const app = express();
 const port = Number(process.env.API_PORT);
+
+if (process.env.NODE_ENV === "production") {
+	Sentry.init({
+		dsn: "https://glet_4aa313505f2ab7f4bb992102d99bbc1b@observe.gitlab.com:443/errortracking/api/v1/projects/42963773",
+		integrations: [
+			new Sentry.Integrations.Http({ tracing: true }),
+			new Sentry.Integrations.Express({ app }),
+		],
+		tracesSampleRate: 0.2,
+		profilesSampleRate: 0.2,
+	});
+	app.use(Sentry.Handlers.requestHandler());
+	app.use(Sentry.Handlers.tracingHandler());
+	app.use(Sentry.Handlers.errorHandler());
+}
 
 app.use(cors({origin: "*"}));
 app.use(express.json());
