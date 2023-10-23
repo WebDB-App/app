24b038816725588b3acfcba3350e583a4474e8e3
biche <aleluff@gmail.com>
2023-10-23 12:18:33 +0200

    🔥 refactor(controller.js): comment out unused imports and code in the list method to improve code readability and maintainability 🔥 refactor(controller.js): comment out unused code in the list method to improve code readability and maintainability

--- back/src/endpoint/version/controller.js
+++ back/src/endpoint/version/controller.js
@@ -1,11 +1,11 @@
-const http = require("../../shared/http.js");
-const version = require("../../shared/version.js");
+//const http = require("../../shared/http.js");
+//const version = require("../../shared/version.js");
 
 class Controller {
 
 	async list(req, res) {
-		const [driver] = await http.getLoggedDriver(req);
-
+		//const [driver] = await http.getLoggedDriver(req);
+		res.send();
 		//version
 		//res.send(await driver.dropRelation(req.body.relation));
 	}
