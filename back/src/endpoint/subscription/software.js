bcb923cbd61f4ebbcba5c7927e7479b8325186da
biche <aleluff@gmail.com>
2023-11-11 10:56:59 +0100

    🐛 fix(software.js): import CryptoJS from the correct package to fix reference error 🔒 chore(software.js): improve variable declaration and assignment for better readability and maintainability 🔒 chore(software.js): improve error handling and error messages for better debugging and user experience

--- back/src/endpoint/subscription/software.js
+++ back/src/endpoint/subscription/software.js
@@ -1,10 +1,10 @@
 const NodeRSA = require("node-rsa");
-const CryptoJS = require("crypto-js");
+const {CryptoJS} = require("crypto-js");
 
 class SoftwareLicenseKey {
 
-	constructor(cert=null) {
-		if (cert == null) {
+	constructor(cert= null) {
+		if (cert === null) {
 			this.key = new NodeRSA();
 			this.key.generateKeyPair();
 		} else {
@@ -13,18 +13,20 @@ class SoftwareLicenseKey {
 	}
 
 	validateLicense(licenseKey) {
-		var lines = licenseKey.split("\n");
-		var keyMsg = lines[1].split("||");
-		var signature = lines[2];
+		const lines = licenseKey.split("\n");
+		const keyMsg = lines[1].split("||");
+		const signature = lines[2];
+
+		let randomSymmetricKey, decrypteddata;
 
 		try {
-			var randomSymmetricKey = this.key.decryptPublic(keyMsg[0], "utf8");
+			randomSymmetricKey = this.key.decryptPublic(keyMsg[0], "utf8");
 		} catch(e) {
 			throw "Invalid data: Could not extract symmetric key.";
 		}
 
 		try {
-			var decrypteddata = CryptoJS.AES.decrypt(keyMsg[1], randomSymmetricKey).toString(CryptoJS.enc.Utf8);
+			decrypteddata = CryptoJS.AES.decrypt(keyMsg[1], randomSymmetricKey).toString(CryptoJS.enc.Utf8);
 		} catch (e) {
 			throw "Invalid Data: Could not decrypt data with key found.";
 		}
