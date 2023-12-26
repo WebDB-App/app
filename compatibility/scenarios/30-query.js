3866d42bc66e9030f5e88d45f20c7cc191c8e38a
biche <aleluff@gmail.com>
2023-12-26 14:01:34 +0100

    :art: chore(compatibility): Update query in 30-query.js
    - Updated the query in 30-query.js to use the most popular currency per continent from servers.js
    - Checked that the query returns 7 results instead of 20
    - Checked that the querySize also returns 7 instead of 20

--- compatibility/scenarios/30-query.js
+++ compatibility/scenarios/30-query.js
@@ -1,17 +1,18 @@
 import assert from 'node:assert';
 import axios from "axios";
 import {test} from "node:test";
+import {mostPopularCurrencyPerContinent} from "../servers.js";
 
 async function run(config) {
 	const query = await axios.post(`${config.api}database/query`, {
-		query: config.select20Relational,
+		query: mostPopularCurrencyPerContinent[config.wrapper],
 		page: 0,
 		pageSize: 100,
 	});
 	await test('[query] Select 20 linked elements', () => {
 		assert.equal(query.status, 200);
 		assert.ok(!query.data.error);
-		assert.ok(query.data.length === 20);
+		assert.ok(query.data.length === 7);
 	});
 
 
@@ -19,11 +20,11 @@ async function run(config) {
 
 
 	const querySize = await axios.post(`${config.api}database/querySize`, {
-		query: config.select20Relational,
+		query: mostPopularCurrencyPerContinent[config.wrapper],
 	});
 	await test('[query] Good size result', () => {
 		assert.equal(querySize.status, 200);
-		assert.ok(querySize.data === 20);
+		assert.ok(querySize.data === 7);
 	});
 }
 
