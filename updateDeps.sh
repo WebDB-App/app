01d2e968a8cf84b1e6096139e07a2c416c92cfa0
biche <aleluff@gmail.com>
2024-09-28 09:54:32 +0200

    Update deps

--- updateDeps.sh
+++ updateDeps.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 #npm i -g dts-bundle-generator
 
+cd front
+
+rm -fr ./node_modules || echo
+rm -fr pnpm-lock.yaml
+pnpm i
+
+(cd ./node_modules/mongodb/ && dts-bundle-generator ./mongodb.d.ts && cp -fr mongodb.d.d.ts ../../../../../src/assets/libs/mongo.d.ts)
+(cd ./node_modules/bson/ && dts-bundle-generator ./bson.d.ts && cp -fr bson.d.d.ts ../../../../../src/assets/libs/bson.d.ts)
+
+(cd ./node_modules/@ngneat/falso/ && dts-bundle-generator --no-check ./src/index.d.ts --external-inlines=@ngneat/falso && cp -fr ./src/index.d.d.ts ../../../../../../src/assets/libs/falso.d.ts)
+(cd ./node_modules/@faker-js/faker/ && dts-bundle-generator --no-check ./dist/types/index.d.ts --external-inlines=@faker-js/faker && cp -fr ./dist/types/index.d.d.ts ../../../../../../src/assets/libs/faker.d.ts)
+
+cd ../
+
+###############
+
+
 cd back
 rm -fr ./node_modules || echo
 rm -fr package-lock.json
@@ -24,15 +41,3 @@ npm i
 cd ../../
 
 ###############
-
-cd front
-
-rm -fr ./node_modules || echo
-rm -fr pnpm-lock.yaml
-pnpm i
-
-cd ./node_modules/mongodb/ && dts-bundle-generator ./mongodb.d.ts && cp -fr mongodb.d.d.ts ../../../../../src/assets/libs/mongo.d.ts &
-cd ./node_modules/bson/ && dts-bundle-generator ./bson.d.ts && cp -fr bson.d.d.ts ../../../../../src/assets/libs/bson.d.ts &
-
-cd ./node_modules/@ngneat/falso/ && dts-bundle-generator --no-check ./src/index.d.ts --external-inlines=@ngneat/falso && cp -fr ./src/index.d.d.ts ../../../../../../src/assets/libs/falso.d.ts &
-cd ./node_modules/@faker-js/faker/ && dts-bundle-generator --no-check ./dist/types/index.d.ts --external-inlines=@faker-js/faker && cp -fr ./dist/types/index.d.d.ts ../../../../../../src/assets/libs/faker.d.ts &
